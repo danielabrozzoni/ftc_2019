ac44d417541092c66a4be73dd1063e6904a7b02c
Daniela Brozzoni <brozzoni@live.it>
2019-02-04 15:22:57 +0100

    Add avanti, indietro, dx, sx

--- TeamCode/src/main/java/org/pizzapastarobottino/ftc/teamcode/OpModes/UserControlled/OpenDay.java
+++ TeamCode/src/main/java/org/pizzapastarobottino/ftc/teamcode/OpModes/UserControlled/OpenDay.java
@@ -1,5 +1,7 @@
 package org.pizzapastarobottino.ftc.teamcode.OpModes.UserControlled;
 
+import android.util.Pair;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -25,10 +27,10 @@ public class OpenDay extends OpMode {
     private final int POSTERIORE_DX = 2;
     private final int POSTERIORE_SX = 3;
 
-    private final double ANT_SX_POWER = 0.1;
-    private final double ANT_DX_POWER = 0.2;
-    private final double POS_SX_POWER = 0.1;
-    private final double POS_DX_POWER = 0.4;
+    private final double ANT_SX_POWER = 0.01;
+    private final double ANT_DX_POWER = 0.01;
+    private final double POS_SX_POWER = 0.01;
+    private final double POS_DX_POWER = 0.01;
 
     @Override
     public void init() {
@@ -51,7 +53,58 @@ public class OpenDay extends OpMode {
         telemetry.addLine("Dpad -> Su: " + gamepad2.dpad_up + " giu: " + gamepad2.dpad_down + " sx: " + gamepad2.dpad_left + " dx: " + gamepad2.dpad_right);
         telemetry.addLine("Bumpers -> sx " + gamepad2.left_bumper + " dx: " + gamepad2.right_bumper);
         telemetry.addLine("Trigger -> sx: " + gamepad2.left_trigger +  " dx: " + gamepad2.right_trigger);
+    }
 
+    private void indietro(float potenza) {
+        powers[ANTERIORE_DX] = -ANT_DX_POWER * potenza;
+        powers[POSTERIORE_SX] = -POS_SX_POWER * potenza;
+        powers[ANTERIORE_SX] = -ANT_SX_POWER * potenza;
+        powers[POSTERIORE_DX] = -POS_DX_POWER * potenza;
+    }
+
+    private void avanti(float potenza) {
+
+        powers[ANTERIORE_DX] = ANT_DX_POWER * potenza;
+        powers[POSTERIORE_SX] = POS_SX_POWER * potenza;
+        powers[ANTERIORE_SX] = ANT_SX_POWER * potenza;
+        powers[POSTERIORE_DX] = POS_DX_POWER * potenza;
+    }
+
+    private void sinistra(float potenza) {
+        powers[ANTERIORE_DX] = -ANT_DX_POWER * potenza;
+        powers[POSTERIORE_SX] = -POS_SX_POWER * potenza;
+        powers[ANTERIORE_SX] = ANT_SX_POWER * potenza;
+        powers[POSTERIORE_DX] = POS_DX_POWER * potenza;
+    }
+
+    private void destra(float potenza) {
+        powers[ANTERIORE_DX] = ANT_DX_POWER * potenza;
+        powers[POSTERIORE_SX] = POS_SX_POWER * potenza;
+        powers[ANTERIORE_SX] = -ANT_SX_POWER * potenza;
+        powers[POSTERIORE_DX] = -POS_DX_POWER * potenza;
+    }
+
+    private void trasla(float x, float y) {
+
+        float r = x * x + y * y;
+
+        if(r < 0.3)
+            return;
+
+        y = -y;
+        double angolo = Math.toDegrees(Math.atan2(y, x));
+        telemetry.addLine("Arcatan: " + angolo);
+
+
+        if(angolo >= 67.5 && angolo < 112.5) {          //su
+            avanti(r);
+        } else if(angolo >= -112.5 && angolo < -67.5) {   // giu
+            indietro(r);
+        } else if(angolo >= -22.5 && angolo < 22.5) {
+            destra(r);
+        } else if(angolo >= 157.5 || angolo <= -157.5) {
+            sinistra(r);
+        }
     }
 
     private void trasla(boolean up, boolean down, boolean l, boolean r) {
@@ -59,70 +112,46 @@ public class OpenDay extends OpMode {
         if(up && down && l && r) return;
 
         if(up) {
-            if(l) {
-                powers[ANTERIORE_DX] = -ANT_DX_POWER;
-                powers[POSTERIORE_SX] = POS_SX_POWER;
-            } else if(r) {
-                powers[ANTERIORE_SX] = ANT_SX_POWER;
-                powers[POSTERIORE_DX] = -POS_DX_POWER;
-            } else {
-                powers[ANTERIORE_DX] = -ANT_DX_POWER;
-                powers[POSTERIORE_SX] = POS_SX_POWER;
-                powers[ANTERIORE_SX] = ANT_SX_POWER;
-                powers[POSTERIORE_DX] = -POS_DX_POWER;
-            }
-        }
-
-        else if(down) {
             if(r) {
                 powers[ANTERIORE_DX] = ANT_DX_POWER;
-                powers[POSTERIORE_SX] = -POS_SX_POWER;
+                powers[POSTERIORE_SX] = POS_SX_POWER;
             } else if(l) {
-                powers[ANTERIORE_SX] = -ANT_SX_POWER;
+                powers[ANTERIORE_SX] = ANT_SX_POWER;
                 powers[POSTERIORE_DX] = POS_DX_POWER;
             } else {
                 powers[ANTERIORE_DX] = ANT_DX_POWER;
+                powers[POSTERIORE_SX] = POS_SX_POWER;
+                powers[ANTERIORE_SX] = ANT_SX_POWER;
+                powers[POSTERIORE_DX] = POS_DX_POWER;
+            }
+        } else if(down) {
+            if(l) {
+                powers[ANTERIORE_DX] = -ANT_DX_POWER;
+                powers[POSTERIORE_SX] = -POS_SX_POWER;
+            } else if(r) {
+                powers[ANTERIORE_SX] = -ANT_SX_POWER;
+                powers[POSTERIORE_DX] = -POS_DX_POWER;
+            } else {
+                powers[ANTERIORE_DX] = -ANT_DX_POWER;
                 powers[POSTERIORE_SX] = -POS_SX_POWER;
                 powers[ANTERIORE_SX] = -ANT_SX_POWER;
-                powers[POSTERIORE_DX] = POS_DX_POWER;
+                powers[POSTERIORE_DX] = -POS_DX_POWER;
             }
         } else if(r) {
             powers[ANTERIORE_DX] = ANT_DX_POWER;
-            powers[POSTERIORE_SX] = -POS_SX_POWER;
-            powers[ANTERIORE_SX] = ANT_SX_POWER;
+            powers[POSTERIORE_SX] = POS_SX_POWER;
+            powers[ANTERIORE_SX] = -ANT_SX_POWER;
             powers[POSTERIORE_DX] = -POS_DX_POWER;
         } else if(l) {
             powers[ANTERIORE_DX] = -ANT_DX_POWER;
-            powers[POSTERIORE_SX] = POS_SX_POWER;
-            powers[ANTERIORE_SX] = -ANT_SX_POWER;
+            powers[POSTERIORE_SX] = -POS_SX_POWER;
+            powers[ANTERIORE_SX] = ANT_SX_POWER;
             powers[POSTERIORE_DX] = POS_DX_POWER;
         }
     }
 
-    private void trasla(double delta, double x, double y){
-        double[] powers2;
-        double temp = -x;
-        x = -y;
-        y = temp;
-
-        try {
-            powers2 = MechanumWheels.getPowerFast(delta,
-                    Math.atan2(
-                            y * Configs.yDirectionFactor,
-                            x * Configs.xDirectionFactor
-                    ));
-            //telemetry.addLine(String.valueOf(powers2[0]));
-            //telemetry.addLine(String.valueOf(powers2[1]));
-            powers[ANTERIORE_DX] = powers[POSTERIORE_DX] = powers2[0];
-            powers[ANTERIORE_SX] = powers[POSTERIORE_SX] = powers2[1];
-        } catch (UndeliverablePowerException e) {
-            telemetry.addLine(e.getMessage());
-            telemetry.update();
-        }
-    }
-
     private void giraSuTeStesso(double delta){
-        powers[ANTERIORE_SX] = powers[POSTERIORE_SX] = delta;
+        powers[ANTERIORE_SX] = powers[POSTERIORE_SX] = -delta;
         powers[ANTERIORE_DX] = powers[POSTERIORE_DX] = delta;
     }
 
@@ -150,21 +179,25 @@ public class OpenDay extends OpMode {
             return;
         }
 
-        if (Math.abs(gamepad2.right_stick_x) > 0.1) {
-            giraSuTeStesso(gamepad2.right_stick_x);
+        trasla(gamepad2.left_stick_x, gamepad2.left_stick_y);
+
+        if(Math.abs(gamepad2.right_stick_x) > 0.1) {
+          giraSuTeStesso(gamepad2.right_stick_x);
         }
 
 
-        trasla(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.dpad_left, gamepad2.dpad_right);
+        //trasla(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.dpad_left, gamepad2.dpad_right);
 
         aggiorna();
-
     }
 
     private void aggiorna() {
+
         robot.getMotor(Configs.motorRuotaAnterioreDX).move(powers[ANTERIORE_DX] * Configs.ruotaAnterioreDXrotationFactor);
-        robot.getMotor(Configs.motorRuotaAnterioreSX).move(powers[ANTERIORE_SX] * Configs.ruotaAnterioreSXrotationFactor);
         robot.getMotor(Configs.motorRuotaPosterioreDX).move(powers[POSTERIORE_DX] * Configs.ruotaPosterioreDXrotationFactor);
+        robot.getMotor(Configs.motorRuotaAnterioreSX).move(powers[ANTERIORE_SX] * Configs.ruotaAnterioreSXrotationFactor);
         robot.getMotor(Configs.motorRuotaPosterioreSX).move(powers[POSTERIORE_SX] * Configs.ruotaPosterioreSXrotationFactor);
+
+        // NB: appena lo fai per il braccio, togli il '* potenza'
     }
 }
